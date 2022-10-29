efafc8f5df42ab405cc921f850cb83b9541a2b25
unknown <sadafzillani98@gmail.com>
2022-10-29 16:11:47 +0530

    photo added

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 
-const categories = require('./category.json');
+const categories = require("./category.json");
+const courses = require("./courses.json");
 
-app.get('/category', (req, res) => {
-    res.send(categories)
-})
+app.get("/category", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/courses/info/:id", (req, res) => {
+  const { id } = req.params;
+  const selectedCourses = courses.filter((course) => course.category == id);
+  res.send(selectedCourses);
+});
 app.listen(port, () => {
-    console.log(`Running on ${port}`)
-})
+  console.log(`Running on ${port}`);
+});
